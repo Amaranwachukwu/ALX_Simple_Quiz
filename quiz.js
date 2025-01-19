9a4cf77067d01984bdb19bf1b4b1d8dcd3dd71b8
Amaranwachukwu <blessingnwachukwu908@gmail.com>
2025-01-19 17:08:40 +0100

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,1 +1,6 @@
-
+function checkAnswer() {
+  //function correct answer
+  const correctAnswer = "4";
+}
+const selectedOption = document.querySelector('input[name="quiz"]:checked');
+    const userAnswer = selectedOption ? selectedOption.value : null;
